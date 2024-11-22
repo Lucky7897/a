55d36b9c9c19185e7fc8219c4366e224f24f9db6
Lucky7897 <nbon123@outlook.com>
2024-11-23 00:44:06 +0100

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,64 +1,255 @@
+
+### `install_linux_gui.sh`
+
+```bash
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 
-   exit 1
+# Script to install GUI on a Linux command-line machine
+
+# Ensure the script is run with superuser privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
-# Step 1: Adjust SSH to use username/password instead of key
-echo "Configuring SSH to use username/password authentication only..."
-sudo sed -i 's/#\?\(PasswordAuthentication\s*\).*/\1yes/' /etc/ssh/sshd_config
-sudo sed -i 's/#\?\(PubkeyAuthentication\s*\).*/\1no/' /etc/ssh/sshd_config
-sudo sed -i 's/#\?\(ChallengeResponseAuthentication\s*\).*/\1no/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# Prompt for username and password
+echo "Please enter your username:"
+read username
+echo "Please enter your password:"
+read -s password
 
-# Step 2: Install and configure VNC
-echo "Installing and configuring VNC server..."
-sudo apt-get update
-sudo apt-get install -y tigervnc-standalone-server xfce4 xfce4-goodies
-vncpasswd
-echo "startxfce4 &" > ~/.vnc/xstartup
-chmod +x ~/.vnc/xstartup
-vncserver :1
+# List of top 15 Linux distributions
+declare -a distros=("Ubuntu" "Debian" "Fedora" "CentOS" "Arch Linux" "OpenSUSE" "Kali Linux" "Linux Mint" "Manjaro" "Pop!_OS" "Elementary OS" "Zorin OS" "MX Linux" "Solus" "PCLinuxOS")
 
-# Step 3: Install top 10 tools for Ubuntu
-echo "Installing top 10 tools for Ubuntu..."
-sudo apt-get install -y htop curl git vim net-tools ufw wget tmux tree build-essential
+# Function to update package list and install GUI based on distro
+install_gui() {
+    case $1 in
+        "Ubuntu"|"Debian")
+            apt-get update
+            apt-get install -y ubuntu-desktop
+            ;;
+        "Fedora")
+            dnf install -y @cinnamon-desktop-environment
+            ;;
+        "CentOS")
+            yum groupinstall -y "GNOME Desktop"
+            ;;
+        "Arch Linux")
+            pacman -Syu
+            pacman -S --noconfirm gnome
+            ;;
+        "OpenSUSE")
+            zypper install -t pattern gnome
+            ;;
+        # Add other distros here...
+        *)
+            echo "Distribution not supported yet."
+            ;;
+    esac
+}
 
-# Step 4: Install the most lightweight GUI
-echo "Installing the lightweight XFCE GUI..."
-sudo apt-get install -y xfce4 xfce4-goodies
+# Prompt for Linux distribution
+echo "Which Linux do you want to install GUI on? (Choose a number)"
+select distro in "${distros[@]}"; do
+    echo "Installing GUI for $distro..."
+    install_gui $distro
+    break
+done
 
-# Step 5: Optimize Ubuntu Server for performance
-echo "Optimizing system performance..."
+# Prompt for SSH configuration
+echo "Do you want to adjust SSH to public key or to username/password authorization? (publickey/usernamepassword)"
+read ssh_config
 
-# Disable unnecessary services
-echo "Disabling unnecessary services..."
-sudo systemctl disable bluetooth.service
-sudo systemctl disable cups.service
-sudo systemctl disable ModemManager.service
+# Function to adjust SSH settings
+adjust_ssh() {
+    case $1 in
+        "publickey")
+            sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+            systemctl restart sshd
+            ;;
+        "usernamepassword")
+            sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+            systemctl restart sshd
+            ;;
+        *)
+            echo "Invalid option. SSH settings not changed."
+            ;;
+    esac
+}
 
-# Tune the filesystem for performance
-echo "Tuning filesystem..."
-sudo tune2fs -o journal_data_writeback /dev/sda1
-sudo mount -o remount,noatime,nodiratime / # For ext4
+adjust_ssh $ssh_config
 
-# Adjust swappiness (use less swap)
-echo "Adjusting swappiness..."
-sudo sysctl vm.swappiness=10
-echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
+# Prompt for RDP/VNC activation
+echo "Do you want to activate RDP or VNC? (rdp/vnc/none)"
+read remote_access
 
-# Enable UFW firewall with basic rules
-echo "Setting up UFW firewall..."
-sudo ufw allow OpenSSH
-sudo ufw allow 5901/tcp  # Allow VNC
-sudo ufw --force enable
+# Function to install and configure RDP/VNC
+setup_remote_access() {
+    case $1 in
+        "rdp")
+            case $distro in
+                "Ubuntu"|"Debian")
+                    apt-get install -y xrdp
+                    systemctl enable xrdp
+                    systemctl start xrdp
+                    ;;
+                "Fedora")
+                    dnf install -y xrdp
+                    systemctl enable xrdp
+                    systemctl start xrdp
+                    ;;
+                "CentOS")
+                    yum install -y epel-release
+                    yum install -y xrdp
+                    systemctl enable xrdp
+                    systemctl start xrdp
+                    ;;
+                "Arch Linux")
+                    pacman -S --noconfirm xrdp
+                    systemctl enable xrdp
+                    systemctl start xrdp
+                    ;;
+                "OpenSUSE")
+                    zypper install -y xrdp
+                    systemctl enable xrdp
+                    systemctl start xrdp
+                    ;;
+                # Add other distros here...
+                *)
+                    echo "RDP not supported for this distribution."
+                    ;;
+            esac
+            ;;
+        "vnc")
+            case $distro in
+                "Ubuntu"|"Debian")
+                    apt-get install -y tightvncserver
+                    ;;
+                "Fedora")
+                    dnf install -y tigervnc-server
+                    ;;
+                "CentOS")
+                    yum install -y tigervnc-server
+                    ;;
+                "Arch Linux")
+                    pacman -S --noconfirm tigervnc
+                    ;;
+                "OpenSUSE")
+                    zypper install -y tigervnc
+                    ;;
+                # Add other distros here...
+                *)
+                    echo "VNC not supported for this distribution."
+                    ;;
+            esac
+            ;;
+        "none")
+            echo "No remote access tools will be installed."
+            ;;
+        *)
+            echo "Invalid option. No remote access tools will be installed."
+            ;;
+    esac
+}
 
-# Clean up
-echo "Cleaning up unnecessary files..."
-sudo apt-get autoremove -y
-sudo apt-get clean
+setup_remote_access $remote_access
 
-# Display final message
-echo "Setup complete! SSH is now using password authentication. VNC server is running on :1 (port 5901). Lightweight XFCE GUI is installed and system is optimized for performance."
+# Prompt for control panel installation
+declare -a control_panels=("Webmin" "cPanel" "Plesk" "ISPConfig" "Ajenti" "VestaCP" "CentOS Web Panel" "Froxlor" "DirectAdmin" "CyberPanel")
+
+echo "Do you want to install a control panel? (Choose a number or type 'none')"
+select control_panel in "${control_panels[@]}" "none"; do
+    case $control_panel in
+        "Webmin")
+            # Installation steps for Webmin
+            wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+            echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
+            apt-get update
+            apt-get install -y webmin
+            ;;
+        "cPanel")
+            # Installation steps for cPanel
+            cd /home
+            curl -o latest -L https://securedownloads.cpanel.net/latest
+            sh latest
+            ;;
+        "Plesk")
+            # Installation steps for Plesk
+            sh <(curl http://autoinstall.plesk.com/one-click-installer)
+            ;;
+        "ISPConfig")
+            # Installation steps for ISPConfig
+            wget -O - https://get.ispconfig.org | sh
+            ;;
+        "Ajenti")
+            # Installation steps for Ajenti
+            apt-get update && apt-get install -y wget apt-transport-https
+            wget http://repo.ajenti.org/debian/key -O- | apt-key add -
+            echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> /etc/apt/sources.list
+            apt-get update
+            apt-get install -y ajenti
+            ;;
+        "VestaCP")
+            # Installation steps for VestaCP
+            curl -O http://vestacp.com/pub/vst-install.sh
+            bash vst-install.sh
+            ;;
+        "CentOS Web Panel")
+            # Installation steps for CentOS Web Panel
+            cd /usr/local/src
+            wget http://centos-webpanel.com/cwp-el7-latest
+            sh cwp-el7-latest
+            ;;
+        "Froxlor")
+            # Installation steps for Froxlor
+            apt-get install -y froxlor
+            ;;
+        "DirectAdmin")
+            # Installation steps for DirectAdmin
+            wget -O setup.sh https://www.directadmin.com/setup.sh
+            chmod 755 setup.sh
+            ./setup.sh auto
+            ;;
+        "CyberPanel")
+            # Installation steps for CyberPanel
+            sh <(curl -s https://cyberpanel.net/install.sh)
+            ;;
+        "none")
+            echo "No control panel will be installed."
+            break
+            ;;
+        *)
+            echo "Invalid option. No control panel will be installed."
+            break
+            ;;
+    esac
+done
+
+# Prompt for popular packages/toolkits installation
+declare -a packages=("Git" "Docker" "Node.js" "Python" "Java" "Ruby" "PHP" "MySQL" "PostgreSQL" "Nginx" "Apache" "MongoDB" "Redis" "Elasticsearch" "Kubernetes")
+
+echo "Do you want to install popular packages/toolkits? (Choose a number or type 'none')"
+select package in "${packages[@]}" "none"; do
+    case $package in
+        "Git")
+            apt-get install -y git
+            ;;
+        "Docker")
+            apt-get install -y docker.io
+            ;;
+        "Node.js")
+            apt-get install -y nodejs npm
+            ;;
+        "Python")
+            apt-get install -y python3 python3-pip
+            ;;
+        "Java")
+            apt-get install -y default-jdk
+            ;;
+        "Ruby")
+            apt-get install -y ruby
+            ;;
+        "PHP")
+            apt-get install -y php libapache2-mod-php
+            ;;
+       
